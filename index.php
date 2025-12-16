4fb067054cb40aeb885fc57c90345ce57dd6f628
Alex Christian <alexanderechristian@gmail.com>
2025-12-16 16:28:30 -0500

    Added closing tag to buttons. Added output rendering.

--- index.php
+++ index.php
@@ -14,9 +14,14 @@ include 'script_runner.php';
     <h1>Asterisk WebUI</h1>
     <form method="post">
     <?php foreach ($scripts as $name => $path): ?>
-        <button type="submit" name="run_script" value="<?= htmlspecialchars($name) ?>"><?= htmlspecialchars($name) ?><br><br>
+        <button type="submit" name="run_script" value="<?= htmlspecialchars($name) ?>"><?= htmlspecialchars($name) ?></button><br><br>
     <?php endforeach; ?>
     <button type="submit" name="reset" value="1">Reset Output</button>
     </form>
+
+		<?php if ($output): ?>
+				<h2>Output:</h2>
+				<pre><?= htmlspecialchars($output) ?></pre>
+		<?php endif; ?>
 </body>
 </html>
