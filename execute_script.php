4626a862a192a04335d52858f140dba0f42a3884
Alex Christian <alexanderechristian@gmail.com>
2026-01-07 12:10:11 -0500

    API endpoint to run scripts

---
+++ execute_script.php
@@ -0,0 +1,21 @@
+<?php
+require 'script_runner.php';
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+$script = $data['script'] ?? null;
+$inputs = $data['inputs'] ?? [];
+
+if (!$script || !isset($scripts[$script])) {
+		http_response_code(400);
+		echo "Invalid script.";
+		exit;
+}
+
+// Just show what will be run for now
+echo "Script: $script\n";
+echo "Inputs:\n";
+
+foreach ($inputs as $i => $input) {
+		echo "   [$i] $input\n";
+}
